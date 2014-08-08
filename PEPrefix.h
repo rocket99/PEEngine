3571d55400178435856779933cbc0a9df2f9f56c
rocket99 <rocket99@126.com>
2014-08-08 21:38:39 +0800

    add prefix.h

---
+++ PEPrefix.h
@@ -0,0 +1,21 @@
+#ifndef __PEPrefix_H__
+#define __PEPrefix_H__
+
+#include <stido.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+
+#ifdef __cplusplus
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+using namespace std;
+#endif
+
+
+
+#endif
